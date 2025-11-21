3d04bca10f5e95dc3b341f01cf58a69f4d89e295
JDamianDelgado <joakodelgado2013@gmail.com>
2025-11-21 00:37:27 -0300

    datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./index.css";
 
-import { Chat } from "./Components/chat.jsx";
+import { Chat } from "./Components/chat";
 import { ChatScreen } from "./Pages/ChatScreen";
 
 function App() {
